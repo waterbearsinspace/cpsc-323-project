2e2080e34d3f01180437ced79f8c3484c21098f3
nicolasviste <nicolasviste@gmail.com>
2024-04-25 22:19:49 -0700

    Separate dollar sign and price, change example product

--- php/page-elements/item.php
+++ php/page-elements/item.php
@@ -1,10 +1,10 @@
 <div class="item-container">
 
-    <img class="item-image" src="https://m.media-amazon.com/images/I/71AIv5OOuNL._AC_UF894,1000_QL80_DpWeblab_.jpg">
+    <img class="item-image" src="https://media-assets.grailed.com/prd/listing/temp/dae8f4677a6643249b26e5adfb828ea6?w=240">
 
     <div class="item-name">this is the name of an item it is very cool</div>
     <div class="purchase-info-container">
-        <div class="price">$200</div>
+        <div class="price"><div>$</div><div>180</div></div>
         <div class="to-cart">♥</div>
     </div>
 </div>
