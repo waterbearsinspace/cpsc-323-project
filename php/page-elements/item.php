fb4a9486a153ec848b608b46588f0cb1115250cd
waterbearsinspace <nicolasviste@gmail.com>
2024-04-26 04:18:45 -0700

    Begin work on items

---
+++ php/page-elements/item.php
@@ -0,0 +1,6 @@
+<div class ="item-container">
+
+    <img class ="item-image" src="https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-image_large.png?v=1530129081">
+
+    TEXT
+</div>
