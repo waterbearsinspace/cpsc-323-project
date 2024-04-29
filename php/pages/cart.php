774b6badee3e2a8cfce674e4810fa1361e46d8fd
nicolasviste <nicolasviste@gmail.com>
2024-04-29 10:36:15 -0700

    Add add to cart functionality

---
+++ php/pages/cart.php
@@ -0,0 +1,19 @@
+<!doctype html>
+
+<!-- Loads the html header -->
+<?php
+  require_once __DIR__ . "/../page-elements/html-head.php";
+?>
+
+<body>
+
+    <!-- Displays the header -->
+    <?php
+    require_once __DIR__ . "/../page-elements/header/header.php";
+    ?>
+
+    This is the cart.
+    
+</body>
+
+</html>
--- php/pages/game.php
+++ php/pages/game.php
@@ -31,7 +31,14 @@
     <div class="page-game-quantity"><?= $ProductQuantity ?></div>
   </div>
 
-  
+  <br />
+
+  <form action="/cpsc-332-project/php/pages/add-to-cart.php" method="post">
+    <input type="hidden" name="UserID" name="UserID" value=<?= $_SESSION['id'] ?>>
+    <input type="hidden" name="ProductID" name="ProductID" value=<?= $ProductID ?>>
+    <input type="number" name="PurchaseQuantity" name="PurchaseQuantity" min="1" max=<?= $ProductQuantity ?> value="1">
+    <input type="submit" name="add-to-cart" value="Add to Cart">
+  </form>
 
 </body>
 
