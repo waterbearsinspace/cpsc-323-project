b21acc09f0011c5ed5cc6f79d2002961eb8710e8
nicolasviste <nicolasviste@gmail.com>
2024-04-29 18:41:16 -0700

    Add barebones delete from cart functionality through update cart

--- php/sql/cart/edit-cart.php
+++ php/sql/cart/edit-cart.php
@@ -16,8 +16,15 @@
         WHERE UserID=? AND ProductID = ?"); 
         $updateCart->bind_param("iii", $PurchaseQuantity, $UserID, $ProductID); 
 
+        // Prepare delete zero
+        $deleteZeroQuantity = $mysqli->prepare("DELETE FROM carts
+        WHERE UserID=? AND PurchaseQuantity = 0"); 
+        $deleteZeroQuantity->bind_param("i", $UserID); 
+
         // Update cart
         $updateCart->execute();
+        $deleteZeroQuantity->execute();
+
         header("Location: /cpsc-332-project/php/pages/cart.php"); 
 
         // Close the connection 
