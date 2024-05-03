aff4af7347513354da67bdd7ba448476979bb1e9
nicolasviste <nicolasviste@gmail.com>
2024-05-03 00:00:48 -0700

    Fix banner

--- php/page-elements/collections/collection-item.php
+++ php/page-elements/collections/collection-item.php
@@ -1,7 +1,7 @@
 <a href="/cpsc-332-project/php/pages/collections.php?ID=<?= $collectionID ?>"
   class="card-container">
   <div class="collection-banner-image-container">
-    <img class="collection-banner-image"
+    <img class="collection-banner-image2"
     src="/cpsc-332-project/img/<?= $collectionID ?>.png" />
   </div>
   <div class="card-info-container">
